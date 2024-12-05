ea540f9c90e94e1624929b80e4b4c2f2b1a697de
cpojer <christoph.pojer@gmail.com>
2024-12-05 14:52:10 +0900

    Convert the Babel plugin to TypeScript.

---
+++ packages/babel-plugin-fbt/src/JSFbtUtil.tsx
@@ -0,0 +1,60 @@
+import nullthrows from 'nullthrows';
+import type { FbtTableKey } from '../../fbt/src/FbtTable';
+import type {
+  ObjectWithJSFBT,
+  TableJSFBTTree,
+  TableJSFBTTreeLeaf,
+} from './index';
+
+export function coerceToTableJSFBTTreeLeaf(
+  value: Partial<TableJSFBTTreeLeaf>
+): TableJSFBTTreeLeaf | null | undefined {
+  return value &&
+    typeof value === 'object' &&
+    typeof value.desc === 'string' &&
+    typeof value.text === 'string' &&
+    (typeof value.tokenAliases === 'object' || value.tokenAliases == null)
+    ? (value as TableJSFBTTreeLeaf)
+    : null;
+}
+
+function _runOnNormalizedJSFBTLeaves(
+  value: Readonly<TableJSFBTTree>,
+  callback: (leaf: TableJSFBTTreeLeaf) => void
+): void {
+  const leaflet = coerceToTableJSFBTTreeLeaf(value);
+  if (leaflet) {
+    return callback(leaflet);
+  }
+
+  for (const k in value) {
+    _runOnNormalizedJSFBTLeaves(nullthrows(value[k]), callback);
+  }
+}
+
+export function onEachLeaf(
+  phrase: ObjectWithJSFBT,
+  callback: (leaf: TableJSFBTTreeLeaf) => void
+): void {
+  _runOnNormalizedJSFBTLeaves(phrase.jsfbt.t, callback);
+}
+
+/**
+ * Clone `tree` and replace each leaf in the cloned tree with the result of
+ * calling `convertLeaf`.
+ */
+export function mapLeaves<NewLeaf>(
+  tree: Readonly<TableJSFBTTree>,
+  convertLeaf: (leaf: Readonly<TableJSFBTTreeLeaf>) => NewLeaf
+): NewLeaf | Partial<Record<FbtTableKey, NewLeaf>> {
+  const leaflet = coerceToTableJSFBTTreeLeaf(tree);
+  if (leaflet != null) {
+    return convertLeaf(leaflet);
+  }
+
+  const newFbtTree: Partial<Record<FbtTableKey, NewLeaf>> = {};
+  for (const tableKey in tree) {
+    newFbtTree[tableKey] = mapLeaves(tree[tableKey], convertLeaf);
+  }
+  return newFbtTree;
+}
---
+++ packages/babel-plugin-fbt/src/fbt-nodes/FbtNameNode.tsx
@@ -0,0 +1,109 @@
+import { CallExpression, isStringLiteral, stringLiteral } from '@babel/types';
+import invariant from 'invariant';
+import type { CallExpressionArg } from '../FbtUtil';
+import {
+  createFbtRuntimeArgCallExpression,
+  enforceBabelNodeCallExpressionArg,
+  errorAt,
+} from '../FbtUtil';
+import { GENDER_ANY } from '../translate/IntlVariations';
+import type { StringVariationArgsMap } from './FbtArguments';
+import { GenderStringVariationArg } from './FbtArguments';
+import FbtNode from './FbtNode';
+import { FbtNodeType } from './FbtNodeType';
+import type { FromBabelNodeFunctionArgs } from './FbtNodeUtil';
+import {
+  createInstanceFromFbtConstructCallsite,
+  tokenNameToTextPattern,
+} from './FbtNodeUtil';
+
+type Options = {
+  // `BabelNode` representing the `gender` of the fbt:name's value
+  gender: CallExpressionArg;
+  name: string; // Name of the string token,
+  // `BabelNode` representing the `value` of the fbt:name to render on the UI
+  value: CallExpressionArg;
+};
+
+/**
+ * Represents an <fbt:name> or fbt.name() construct.
+ * @see docs/params.md
+ */
+export default class FbtNameNode extends FbtNode<
+  GenderStringVariationArg,
+  CallExpression,
+  null,
+  Options
+> {
+  static readonly type: FbtNodeType = FbtNodeType.Name;
+
+  override getOptions(): Options {
+    try {
+      const { moduleName } = this;
+      let [node, value, gender] = this.getCallNodeArguments() || [];
+
+      invariant(
+        isStringLiteral(node),
+        'Expected first argument of %s.name to be a string literal, but got %s',
+        moduleName,
+        node && node.type
+      );
+      value = enforceBabelNodeCallExpressionArg(
+        value,
+        `Second argument of ${moduleName}.name`
+      );
+      gender = enforceBabelNodeCallExpressionArg(
+        gender,
+        `Third argument of ${moduleName}.name`
+      );
+
+      return { name: node.value, value, gender };
+    } catch (error: any) {
+      throw errorAt(this.node, error);
+    }
+  }
+
+  /**
+   * Create a new class instance given a BabelNode root node.
+   * If that node is incompatible, we'll just return `null`.
+   */
+  static fromBabelNode({
+    moduleName,
+    node,
+  }: FromBabelNodeFunctionArgs): FbtNameNode | null | undefined {
+    return createInstanceFromFbtConstructCallsite(moduleName, node, this);
+  }
+
+  override getArgsForStringVariationCalc(): ReadonlyArray<GenderStringVariationArg> {
+    return [
+      new GenderStringVariationArg(this, this.options.gender, [GENDER_ANY]),
+    ];
+  }
+
+  override getTokenName(_argsMap: StringVariationArgsMap): string {
+    return this.options.name;
+  }
+
+  override getText(argsMap: StringVariationArgsMap): string {
+    try {
+      argsMap.mustHave(this);
+      return tokenNameToTextPattern(this.options.name);
+    } catch (error: any) {
+      throw errorAt(this.node, error);
+    }
+  }
+
+  override getFbtRuntimeArg(): CallExpression {
+    const { gender, name, value } = this.options;
+    return createFbtRuntimeArgCallExpression(
+      this,
+      [stringLiteral(name), value, gender].filter(Boolean)
+    );
+  }
+
+  override getArgsThatShouldNotContainFunctionCallOrClassInstantiation(): Readonly<{
+    [argName: string]: CallExpressionArg;
+  }> {
+    return { gender: this.options.gender };
+  }
+}
